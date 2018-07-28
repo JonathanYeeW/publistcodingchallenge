c141c25f9c84356d22358e853fc7755b74027e22
Jonathan Yee <Jonathan.Yee.W@gmail.com>
2018-07-28 10:23:23 -0700

    updated the rows and header text

--- src/App.js
+++ src/App.js
@@ -29,21 +29,26 @@ class App extends Component {
         }))
       .catch("There was an error w/ makeapicall()")
   }
-  
+
   render() {
     return (
       <div className="App container">
-        <button onClick={() => console.log(this.state.articles)}>COnsole.log articles</button>
-        {this.state.articles["data"] != undefined && <button onClick={() => this.setState({ trigger: true })}>Show Articles</button>}
+        <div className="p-3">
+          <h1>Publist.ai Frontend Coding Challenge</h1>
+          <p>Hello! Thank you for taking the time to review my code submission. In the two hour window I designed a simple front-end using React and Bootstrap4 to fetch the news articles and display them for the user.</p>
+          <p>If I had more time, what I would like to do is have each card be initially show the slug, and if you click on the header the card would expand to see the full article. That way users would be able to see a higher volume of articles and only be shown deeper information for the news that catches their eye.</p>
+          <p>Jonathan Yee, Web Developer</p>
+
+          {this.state.articles["data"] != undefined && <button className="btn btn-secondary" onClick={() => this.setState({ trigger: true })}>Click Here To Show Articles</button>}
+        </div>
         <div className="row">
           {this.state.trigger && this.state.articles["data"].map(item => {
-            return <div className="col-3 mb-2">
+            return <div className="col-12 mb-5">
               <News
                 item={item}
               />
             </div>
           })}
-
         </div>
       </div>
     );
@@ -54,14 +59,19 @@ class News extends Component {
   render() {
     return (
       <div>
-        <div className=" card">
-          <div className="bg-success card-header"><h4>{this.props.item.title}</h4></div>
+        <div className="card">
+          <div className="bg-info card-header text-center"><h5>{this.props.item.title}</h5></div>
           <div className="card-body">
-            <p className="text-muted">{this.props.item.authors}</p>
-            <p>{this.props.item.description}</p>
-          </div>
-          <div className="card-footer">
-            <p>{this.props.item.slug}</p>
+            {this.props.item.authors && <p className="text-muted"> By: {this.props.item.authors}</p>}
+            {this.props.item.description && <p>{this.props.item.description}</p>}
+            <div className="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+              <div className="btn-group mr-2" role="group" aria-label="Second group">
+                <button className="btn btn-outline-info">See More</button>
+              </div>
+              <div className="btn-group mr-2" role="group" aria-label="Second group">
+                <button className="btn btn-outline-info">Share</button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
